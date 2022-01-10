b2103ba8f7d362eaf21d21bbca3a102c5962fe42
kadonomaro <denis.afer@gmail.com>
2022-01-10 20:45:55 +0700

    feat: callback and calc api

--- components/common/TheModal/components/ModalCalc.vue
+++ components/common/TheModal/components/ModalCalc.vue
@@ -8,9 +8,12 @@
         components: { TheModalWrapper, BaseInput, BaseButton },
         data() {
             return {
-                name: "",
-                phone: "",
-                date: "",
+                user: {
+                    name: "",
+                    phone: "",
+                    date: "",
+                },
+                isSuccess: false,
             };
         },
         computed: {
@@ -27,33 +30,68 @@
                 ].join("-");
             },
         },
+        methods: {
+            submitHandler() {
+                const date = new Date(this.user.date).toLocaleDateString(
+                    "ru-RU",
+                    {
+                        day: "numeric",
+                        month: "short",
+                        year: "numeric",
+                    }
+                );
+                const request = JSON.stringify({
+                    name: this.user.name,
+                    phone: this.user.phone,
+                    date,
+                });
+
+                this.$axios
+                    .post("/api/send-calc", request, {
+                        headers: { "content-type": "application/json" },
+                    })
+                    .then(() => {
+                        this.isSuccess = true;
+                    });
+            },
+        },
     };
 </script>
 
 <template>
     <div class="modal-calc">
         <the-modal-wrapper title="Бесплатный замер">
-            <base-input
-                v-model="name"
-                name="name"
-                placeholder="Имя"
-            ></base-input>
-            <base-input
-                v-model="phone"
-                name="phone"
-                placeholder="Номер телефона"
-                type="tel"
-            ></base-input>
-            <base-input
-                v-model="date"
-                name="date"
-                :min="currentDate"
-                placeholder="Дата замера"
-                type="date"
-            ></base-input>
-            <base-button class="modal-calc__button">
-                Вызвать замерщика
-            </base-button>
+            <div v-if="isSuccess" class="modal-calc__success">
+                Спасибо за заявку. Наш оператор свяжется с вами в течение дня
+                для уточнения дополнительной информации
+            </div>
+            <form
+                v-else
+                class="modal-calc__form"
+                @submit.prevent="submitHandler"
+            >
+                <base-input
+                    v-model="user.name"
+                    name="name"
+                    placeholder="Имя"
+                ></base-input>
+                <base-input
+                    v-model="user.phone"
+                    name="phone"
+                    placeholder="Номер телефона"
+                    type="tel"
+                ></base-input>
+                <base-input
+                    v-model="user.date"
+                    name="date"
+                    :min="currentDate"
+                    placeholder="Дата замера"
+                    type="date"
+                ></base-input>
+                <base-button class="modal-calc__button">
+                    Вызвать замерщика
+                </base-button>
+            </form>
         </the-modal-wrapper>
     </div>
 </template>
@@ -62,5 +100,11 @@
     .modal-calc__button {
         width: 100%;
         margin-top: 12px;
+    }
+
+    .modal-calc__success {
+        color: $color-light;
+        font-size: 16px;
+        line-height: 24px;
     }
 </style>
--- components/common/TheModal/components/ModalCallback.vue
+++ components/common/TheModal/components/ModalCallback.vue
@@ -8,9 +8,23 @@
         components: { TheModalWrapper, BaseInput, BaseButton },
         data() {
             return {
-                name: "",
-                phone: "",
+                user: {
+                    name: "",
+                    phone: "",
+                },
+                isSuccess: false,
             };
+        },
+        methods: {
+            submitHandler() {
+                this.$axios
+                    .post("/api/send-callback", JSON.stringify(this.user), {
+                        headers: { "content-type": "application/json" },
+                    })
+                    .then(() => {
+                        this.isSuccess = true;
+                    });
+            },
         },
     };
 </script>
@@ -18,20 +32,29 @@
 <template>
     <div class="modal-callback">
         <the-modal-wrapper title="Обратный звонок">
-            <base-input
-                v-model="name"
-                name="name"
-                placeholder="Имя"
-            ></base-input>
-            <base-input
-                v-model="phone"
-                name="phone"
-                placeholder="Номер телефона"
-                type="tel"
-            ></base-input>
-            <base-button class="modal-callback__button">
-                Заказать звонок
-            </base-button>
+            <div v-if="isSuccess" class="modal-callback__success">
+                Спасибо за заявку. Наш оператор свяжется с вами в течение дня
+            </div>
+            <form
+                v-else
+                class="modal-callback__form"
+                @submit.prevent="submitHandler"
+            >
+                <base-input
+                    v-model="user.name"
+                    name="name"
+                    placeholder="Имя"
+                ></base-input>
+                <base-input
+                    v-model="user.phone"
+                    name="phone"
+                    placeholder="Номер телефона"
+                    type="tel"
+                ></base-input>
+                <base-button class="modal-callback__button">
+                    Заказать звонок
+                </base-button>
+            </form>
         </the-modal-wrapper>
     </div>
 </template>
@@ -40,5 +63,11 @@
     .modal-callback__button {
         width: 100%;
         margin-top: 12px;
+    }
+
+    .modal-callback__success {
+        color: $color-light;
+        font-size: 16px;
+        line-height: 24px;
     }
 </style>
