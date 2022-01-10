2232a0fe3a45060949ca14730be1eb89fcbc248c
kadonomaro <denis.afer@gmail.com>
2022-01-10 20:45:55 +0700

    feat: callback and calc api

--- nuxt.config.js
+++ nuxt.config.js
@@ -92,7 +92,7 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        baseURL: "/",
+        baseURL: "https://api-stretch-ceiling.vercel.app/",
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
